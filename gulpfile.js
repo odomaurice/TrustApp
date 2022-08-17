06ea74021afc4c2f3152915be3915c06c8079a11
Odo Maurice{alphaCodes} <92038525+odomaurice@users.noreply.github.com>
2022-08-17 03:00:29 +0100

    later structural changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 
 function buildStyles() {
-    return src('beta/**/*.scss')
-    .pipe(sass())
-    .pipe(dest('css'))
+    return src("beta/**/*.scss")
+      .pipe(sass())
+      .pipe(purgecss({ content: ["*.html"] }))
+      .pipe(dest("css"));
 }
 
 function watchTask() {
